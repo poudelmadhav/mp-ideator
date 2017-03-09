906d56f21a0df82dfa8cf75e2bbc772a9a45d7c6
Madhav Paudel <poudelmadhav143@gmail.com>
2017-03-09 16:36:23 +0545

    Deleting and editing button added

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -26,6 +26,12 @@ class IdeasController < ApplicationController
 	  end
 	end
 
+	def destroy
+	  @idea = Idea.find(params[:id])
+	  @idea.destroy
+	  redirect_to root_path
+	end
+
 	private
 
 	def idea_params
