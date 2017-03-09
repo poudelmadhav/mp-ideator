5601a7c6d88cdc80c76b0dafa4db3d6c94d5898a
Madhav Paudel <poudelmadhav143@gmail.com>
2017-03-09 11:13:20 +0545

    update and edit form added

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -8,6 +8,15 @@ class IdeasController < ApplicationController
 		redirect_to root_path
 	end
 
+	def edit
+		@idea = Idea.find(params[:id])
+		if @idea.update(idea_params)
+	    	redirect_to root_path
+	  	else
+	    	redirect_to edit_idea_path(params[:id])
+	    end
+	end
+
 	private
 
 	def idea_params
