68fac91f3440c0cfc22a5314f3c160ddc6bcfbb0
Madhav Paudel <poudelmadhav143@gmail.com>
2017-03-09 09:57:10 +0545

    Form styled

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -4,7 +4,14 @@ class IdeasController < ApplicationController
 	end
 
 	def create
-		@idea = Idea.create(description: params[:idea][:description], author: params[:idea][:author])
+		@idea = Idea.create(idea_params)
+		redirect_to root_path
+	end
+
+	private
+
+	def idea_params
+	  params.require(:idea).permit(:description, :author)
 	end
 
 end
