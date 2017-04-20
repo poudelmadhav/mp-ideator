d0c9b8cce71fc7de06deb05e025a45fdcf66e327
Madhav Paudel <poudelmadhav143@gmail.com>
2017-04-20 18:33:10 +0545

    ideas admin interface edited

--- app/admin/idea.rb
+++ app/admin/idea.rb
@@ -1,15 +1,22 @@
 ActiveAdmin.register Idea do
-# See permitted parameters documentation:
-# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-#
-# permit_params :list, :of, :attributes, :on, :model
-#
-# or
-#
-# permit_params do
-#   permitted = [:permitted, :attributes]
-#   permitted << :other if params[:action] == 'create' && current_user.admin?
-#   permitted
-# end
+	menu priority: 3
+	permit_params :author, :description
+
+	index do
+	    selectable_column
+	    id_column
+	    column :author
+	    column :description
+	    column :created_at
+	    actions
+	  end
+
+	form do |f|
+	    f.inputs "Admin Details" do
+	      f.input :author
+	      f.input :description
+	    end
+	    f.actions
+	  end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170416031352) do
+ActiveRecord::Schema.define(version: 20170420110917) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,8 +40,6 @@ ActiveRecord::Schema.define(version: 20170416031352) do
     t.datetime "last_sign_in_at"
     t.inet     "current_sign_in_ip"
     t.inet     "last_sign_in_ip"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
     t.index ["email"], name: "index_admin_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true, using: :btree
   end
